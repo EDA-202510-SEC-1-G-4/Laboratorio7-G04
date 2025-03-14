46f293cba8ad788e101ea75cf79a7238c0d57332
DanRin9 <da.rinconr12@uniandes.edu.co>
2025-03-14 07:59:18 -0500

    CREO Q VOY A FLOTAR

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -11,8 +11,13 @@ def contains():
 def get():
     return
 
-def remove():
-    return 
-
-def size():
-    return
+def remove(map,key): 
+    for elm in map["table"]["elements"]:
+        pos = mp.hash_value(map,elm["key"])
+        if pos == key:
+            elm["key"] = "__EMPTY__"
+            elm["value"] = "__EMPTY__"
+    return map
+                    
+def size(map):
+    return map["size"]
